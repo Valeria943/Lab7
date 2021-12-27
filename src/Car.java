37e39c7c7a7f3a54d3c7688796369d44cfe101c1
Valeria943 <82813302+Valeria943@users.noreply.github.com>
2021-12-27 15:25:21 +0900

    Initial commit

---
+++ src/Car.java
@@ -0,0 +1,17 @@
+import java.io.Serializable;
+public class Car implements Serializable{
+    String model, reg_nom;
+    int year, prob, price;
+
+
+    @Override
+    public String toString() {
+        return "Car{" +
+                "model='" + model + "\' " + 
+                ", reg_nom ' " + reg_nom + '\'' +
+                ", year=" + year +
+                ", prob=" + prob +
+                ", price=" + price +
+                '}';
+    }
+}
